5ec347c3b7614aadef4fa2638426647fb8f4af65
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-11-08 13:59:58 +0600

    bnner section unwanted code remove

--- src/Components/Pages/Banner.jsx
+++ src/Components/Pages/Banner.jsx
@@ -15,28 +15,7 @@ const Banner = () => {
         <div className="absolute hidden md:block md:top-[500px] xl:top-[740px] md:right-12 xl:right-12  text-black  rounded-full font-semibold ">
           <img src={victor} alt="" />
         </div>
-        {/* <div className="flex flex-col md:flex-row py-32 items-center justify-between relative">
-        
-          <div className="absolute top-1/2 left-1/4 transform -translate-y-1/2 -translate-x-1/4 z-10 ">
-            <h1 className="uppercase text-[23px] xl:text-[58px] font-bold bg-[#c52a1e] bg-opacity-30 pr-8 text-white">
-              Taste The Authentic <br />
-              Saudi Cuisine
-            </h1>
 
-            <p className="text-white text-sm sm:text-base md:text-lg lg:text-xl mt-4 border-2 p-2">
-              Among the best Saudi Chefs in the world. Serving you something
-              beyond flavor.
-            </p>
-            <button className="py-2 px-4 bg-[#febf00] uppercase font-bold my-4">
-              Explore Menu
-            </button>
-          </div>
-
-        
-          <div className="relative flex justify-center md:justify-end w-full">
-            <img className="w-[70%]" src={banner} alt="Saudi Cuisine" />
-          </div>
-        </div> */}
         <div className="flex flex-col lg:flex-row items-center justify-center  text-white  ">
           <div className=" lg:text-left  space-y-32 xl:space-y-6 ">
             <h1 className="text-3xl md:text-5xl xl:text-6xl font-bold leading-tight bg-[#c52a1e] bg-opacity-25 z-10 absolute md:-mt-20 xl:-mt-32 pr-8">
